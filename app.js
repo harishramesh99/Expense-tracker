ca14fb99554db3060a6d10d711bf2ca4b72c32fb
harishramesh99 <harish.ramesh99@gmail.com>
2025-02-06 11:52:36 -0600

    basic flow fix hbs-node-mongo

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import { engine } from "express-handlebars";
+import path from "path";
+import { fileURLToPath } from 'url';
+import expense from "./routes/expenses.js";
+import connectDB from './config/db.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.engine("handlebars", engine({ defaultLayout: false }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expense);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
